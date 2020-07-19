a7de65087d3f81857b25837bc8f75f4214f76879
Eddy Sanchez Villanueva <Eddy.Sanchez@pe.ey.com>
2020-07-18 23:49:44 -0500

    Updates

---
+++ precache-manifest.ab539e5ece672c0e51d14bc86e7375fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1863e16d1bac2c8c67f0571adc1cec4",
+    "url": "/dog_breed/index.html"
+  },
+  {
+    "revision": "88429dd3b34ff46f7205",
+    "url": "/dog_breed/static/css/main.1d1af5da.chunk.css"
+  },
+  {
+    "revision": "bb077ab37b5963a0f9ac",
+    "url": "/dog_breed/static/js/2.bf942b87.chunk.js"
+  },
+  {
+    "revision": "b176979c9059f06f5cd16e8ace37dccf",
+    "url": "/dog_breed/static/js/2.bf942b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88429dd3b34ff46f7205",
+    "url": "/dog_breed/static/js/main.5d396429.chunk.js"
+  },
+  {
+    "revision": "cae52f550887454fe06e",
+    "url": "/dog_breed/static/js/runtime-main.72d02549.js"
+  }
+]);
